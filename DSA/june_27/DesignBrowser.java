10932e90c65f4d29974c3bc3b4c9a3551d10bf83
vikram <vk8969338090@gmail.com>
2025-06-29 19:53:05 +0530

    27th june task completed

--- DSA/june_27/DesignBrowser.java
+++ DSA/june_27/DesignBrowser.java
@@ -1,5 +1,68 @@
 package DSA.june_27;
 
 public class DesignBrowser {
+    Node head;
+    Node tail;
+    Node curr;
 
+    public DesignBrowser(String homepage) {
+        head = new Node(homepage);
+        curr = head;
+    }
+
+    public void visit(String url) {
+        Node temp = new Node(url);
+        if (curr.next != null) {
+            curr.next.prev = null;
+        }
+        curr.next = temp;
+        temp.prev = curr;
+        curr = curr.next;
+        tail = curr;
+    }
+
+    public String back(int steps) {
+        int i = 0;
+
+        while (i < steps && curr.prev != null) {
+            // System.out.println(curr.val);
+            curr = curr.prev;
+            i++;
+        }
+        // System.out.println(curr.val);
+        if (curr == null) {
+            curr = head;
+            return head.val;
+        } // System.out.println(curr.val);
+        return curr.val;
+    }
+
+    public String forward(int steps) {
+        int i = 0;
+        // System.out.println(curr.val);
+        while (i < steps && curr.next != null) {
+            curr = curr.next;
+            i++;
+        }
+        if (curr == null) {
+            curr = tail;
+            System.out.println(curr.val);
+            return tail.val;
+        } // System.out.println(curr.val);
+        return curr.val;
+    }
+
+    private class Node {
+        String val;
+        Node prev;
+        Node next;
+
+        private Node(String val) {
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- DSA/june_27/MaximumTwinSum.java
+++ DSA/june_27/MaximumTwinSum.java
@@ -1,5 +1,44 @@
 package DSA.june_27;
 
-public class MaximumTwinSum {
+class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class MaximumTwinSum {
+    public static void main(String[] args) {
+
+    }
+
+    public int pairSum(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode prev = null;
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+        int ans = Integer.MIN_VALUE;
+        ListNode first = head;
+        ListNode second = prev;
+        while (second != null) {
+            int sum = first.val + second.val;
+            ans = Math.max(ans, sum);
+            first = first.next;
+            second = second.next;
+        }
+        return ans;
+    }
 }
